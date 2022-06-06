640947dd04de33bd405949d58242566424b63f4a
Projekt89 <dssd12htc@gmail.com>
2022-06-06 18:13:36 +0200

    DOM traversing, Event propagation, bubbling and capturing, event types and handlers, my version of content tabs

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -66,14 +66,65 @@ learnMoreBtn.addEventListener('click', () => {
   // does same as all code above but works only in modern browsers !!!!!!!!
   section1.scrollIntoView({ behavior: 'smooth' });
 });
+////////////////////////////////////////////////////////////////////
+/* EVENT DELEGATION - IMPLEMENTING SMOOTH SCROLLING TO NAVIGATION */
+////////////////////////////////////////////////////////////////////
+// const navBtns = document.querySelectorAll('.nav__link');
 
+// navBtns.forEach(el =>
+//   el.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
+//   })
+// ); // this kind of attaching listeners is not efficient in bigger node collections
+
+// therefor we will use EVENT DELEGATION which is ATTACHING EVENT TO PARENT NODE AND TRIGGERING IT THROUGH THE MECHANISM OF BUBBLING. This is also usefull functionality for preparing event handling for not yet existing elements
+
+// EVENT DELEGATION PROCEDURE:
+// 1. Add event listener to common parent element
+// 2. Determine what element originated the event
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  e.preventDefault();
+  if (!e.target.classList.contains('nav__link')) return; // prevent event when not link clicked
+
+  document
+    .querySelector(e.target.getAttribute('href')) // E.TARGET POINTS TO TRIGGER ELEMENT
+    .scrollIntoView({ behavior: 'smooth' });
+});
+// despite there is no listener attached to .nav__link element clicking on it triggers event anyway. The event during bubbling 'travels up' the tree and triger 'click' events attached to parent .nav__links list. In the event object we have stored element that triggered  the event so we get required data (id we want to scroll to) from it and perform scrollIntoView.
+
+///////////////////////////////////////////////////////////////////
+/* TAB COMPONENT - EVENT DELEGATION, DOM TRAVERSING, DATA ATTR.  */
+///////////////////////////////////////////////////////////////////
+document.querySelector('.operations').addEventListener('click', function (e) {
+  // selecting correct trigger
+  let trigger = e.target;
+  if (trigger.tagName === 'SPAN') trigger = e.target.parentNode;
+  if (trigger.classList.contains('btn')) {
+    const tabIndex = trigger.dataset.tab;
+    // selecting and manipulation of content
+    this.querySelectorAll('.operations__content').forEach(content => {
+      content.classList.remove('operations__content--active');
+      [...content.parentNode.children][tabIndex].classList.add(
+        'operations__content--active'
+      );
+    });
+    // selecting and manipulation of tab buttons
+    this.querySelectorAll('.operations__tab').forEach(btn =>
+      btn.classList.remove('operations__tab--active')
+    );
+    trigger.classList.add('operations__tab--active');
+  }
+});
 ////////////////////////////////////////
 /* SELECT, CREATE AND DELETE ELEMENTS */
 ////////////////////////////////////////
 let message, logo;
 
 {
-  // SELECTING
+  /* // SELECTING
   console.log(document.documentElement); // selecting document
   console.log(document.head); // selecting head
   console.log(document.body); // selecting body
@@ -105,13 +156,13 @@ let message, logo;
     .querySelector('.btn--close-cookie')
     .addEventListener('click', () => message.remove());
   // In old version before implementation of remove()
-  // message.parentElement.removeChild(message)
+  // message.parentElement.removeChild(message) */
 }
 ////////////////////////////////////
 /* STYLES, ATTRIBUTES AND CLASSES */
 ////////////////////////////////////
 {
-  // STYLES
+  /*   // STYLES
   // Setting inline styles
   message.style.backgroundColor = '#37383d';
   message.style.width = '120%';
@@ -155,13 +206,13 @@ let message, logo;
   // logo.classList.remove('c');
   // logo.classList.toggle('c');
   // logo.classList.contains('c');
-  // logo.className = 'logo'; // don't do this!!! overrites all current classes
+  // logo.className = 'logo'; // don't do this!!! overrites all current classes */
 }
 //////////////////////////////
 /* EVENT TYPES AND HANDLERS */
 //////////////////////////////
 {
-  // Anything that happens within a document generates an event. Even just moving a mouse
+  /* // Anything that happens within a document generates an event. Even just moving a mouse
   // Events are happening no mather wether we are listening for them or not
   const h1 = document.querySelector('h1');
 
@@ -186,5 +237,89 @@ let message, logo;
   }, 4000);
 
   // Setting event listeners directly in HTML - should not be used at all!!!
-  // <h1 onclick="alert('HTML alert') ">Heading on website</h1>
+  // <h1 onclick="alert('HTML alert') ">Heading on website</h1>*/
+}
+////////////////////////////////////////////////
+/* EVENT PROPAGATION - BUBBLING AND CAPTURING */
+////////////////////////////////////////////////
+{
+  /* // When event is triggered by for example click on the link, the event element is not created by this clicked element.
+  //
+  // 1. CAPTURING PHASE - The event element is generated by the document element (root of the DOM tree) and trevels down the elements tree to the element that called it
+  // 2. TARGET PHASE - When the event element reaches the target element the target phase begins where events can be handled right at the target and the function associated with that event is triggered
+  // 3. BUBBLING PHASE - When the event happen the event bubble back up to the root by passing through every PARENT element in the DOM tree between target element and the root
+  //
+  // That mechanism cause that event that triggers by the target is like it would happen in every element that event is passing through. That can cause situation where 'click' event on a button trigers reaction on one of it's parent elements if that parent element is also listening for click event. !!!
+  //
+  // Not all events have capturing and bubbling phase but most of them do
+  //
+  // The mechanisms described above are called PROPAGATION so we can say that event triggered on the element propagates to it's parent elements
+  //
+  // PRACTICE
+
+  const randomInt = (min, max) =>
+    Math.floor(Math.random() * (max - min) + 1 + min);
+  const randomColor = () =>
+    `rgb(${randomInt(0, 255)},${randomInt(0, 255)},${randomInt(0, 255)})`;
+  console.log(randomColor());
+
+  document.querySelector('.nav__link').addEventListener('click', function (e) {
+    this.style.backgroundColor = `${randomColor()}`;
+    console.log('LINK', e.target, e.currentTarget);
+
+    e.stopPropagation(); // to stop propagation we need to explicitly call it in listener !!!!
+    // this is not great idea but can help with unwanted behaviour in complex applications
+  });
+
+  document.querySelector('.nav').addEventListener('click', function (e) {
+    this.style.backgroundColor = `${randomColor()}`;
+    console.log('LINK', e.target, e.currentTarget);
+  });
+
+  document.querySelector('.nav__links').addEventListener(
+    'click',
+    function (e) {
+      this.style.backgroundColor = `${randomColor()}`;
+      console.log('LINK', e.target, e.currentTarget);
+    },
+    true // third arg in listener is if we want to CATCH the event during CAPTURING PHASE
+    // It cause that the event is triggered first while rest is triggered during bubbling phase
+  );
+  // e.target - triggering element,
+  // e.currentTarget - element to which listener is attached and is the same as this keyword
+  // propagation cause triggering of all 3 listeners as nav-links and nav are parents of nav-link.
+  // in all 3 cases after click on link e.target points to link as element that triggered event so this is an example of propagation (bubbling) of events
+
+  // arrow functions prevent propagations */
+}
+/////////////////////////////////////////////
+/* DOM TRAVERSING - PARENT, SIBLING, CHILD */
+/////////////////////////////////////////////
+{
+  /* const h1 = document.querySelector('h1');
+
+  // Going downwards: CHILD
+  console.log(h1.querySelectorAll('.highlight')); // selecting all children of h1 el. with class
+  console.log(h1.childNodes); // NodeList of all h1 children (text, elements, comments, all)
+  console.log(h1.children); // HTMLCollection of only h1 children that are HTML elements
+  h1.firstElementChild.style.color = '#fff'; // selects first HTML child of h1
+  h1.lastElementChild.style.color = 'orangered'; // selects last HTML child of h1
+
+  // Going upwards: PARENT
+  console.log(h1.parentNode); // direct parent element of h1
+  console.log(h1.parentElement); // the same as above
+  h1.closest('.header').style.background = 'var(--gradient-secondary)'; // arg = query same as querySelector. Select nearest element that match query string. Often used for event delegation
+  h1.closest('h1'); // returns itselves as it's closest h1 in this case
+
+  // Going sideways: SIBLINGS
+  console.log(h1.previousElementSibling); // null as h1 has no previous sibling
+  console.log(h1.nextElementSibling); //  h4  - next HTML element sibling is H4
+  console.log(h1.previousSibling); // #text - previous node is text
+  console.log(h1.nextSibling); // #text - next node is also text
+  console.log(h1.parentElement.children); // selecting all sibling HTML elements of h1
+
+  // create array from HTMLCollection and perform operation all h1 siblings
+  [...h1.parentElement.children].forEach(el => {
+    if (el !== h1) el.style.transform = 'scale(0.5)';
+  }); */
 }
